306f6ffdc68b642906b03a28e1c5c1683afd0a3e
giakinh0823 <giakinh2000@gmail.com>
2025-02-03 11:29:39 +0700

    first commit

---
+++ src/main/java/codefod/com/springbootmentor/SpringBootMentorApplication.java
@@ -0,0 +1,13 @@
+package codefod.com.springbootmentor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootMentorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootMentorApplication.class, args);
+    }
+
+}
